306489ec4a6b7fde09466d471fcd9105c34e3cd4
imran1831 <imran807428@gmail.com>
2026-01-10 20:35:41 +0530

    Rotating an Array by k times

--- Rotating_Array.java
+++ Rotating_Array.java
@@ -1,18 +1,25 @@
 import java.util.*;
 class Rotating_Array{
-    public static int[] rotate_by_k(int[] a,int k){
-        int left=0;
-        int right=a.length-1;
-        while(left<right){
-            int temp=a[left];
-            a[left]=a[right];
-            a[right]=temp;
+    public static int[] rotateArr(int arr[], int d) {
+        int n=arr.length;
+        d=d%n;
+        reverse(arr,0,d-1);
+        reverse(arr,d,n-1);
+        reverse(arr,0,n-1);
+        return arr;
+    }
+    public static void reverse(int[] arr,int start,int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
         }
-        return a;
     }
     public static void main(String[] args) {
         int[] arr={1,2,3,4,5};
         int k=2;
-        System.out.println(Arrays.toString(rotate_by_k(arr,k)));
+        System.out.println(Arrays.toString(rotateArr(arr,k)));
     }
 }
