054d81a8b24db3a4339ad8bf1ee215caa8306f2c
imran1831 <imran807428@gmail.com>
2026-01-11 21:33:24 +0530

    Intilize the code for buy and sell stocks

---
+++ BuySellStocks.java
@@ -0,0 +1,9 @@
+public class BuySellStocks {
+    public static int[] caluclat(int[] arr){
+        
+    }
+    public static void main(String[] args) {
+        int[] arr={2,4,6,3,2,8};
+        caluclat(arr);
+    }
+}
