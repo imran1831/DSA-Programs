8467ec0ce2eb4f0d160446ac63a2341dfdb22907
imran1831 <imran807428@gmail.com>
2026-01-11 21:46:07 +0530

    BSS

--- BuySellStocks.java
+++ BuySellStocks.java
@@ -1,6 +1,8 @@
 public class BuySellStocks {
-    public static int[] caluclat(int[] arr){
-        
+    public static int[] caluclat(int[] prices){
+        int i=0;
+        // while()
+        return prices;
     }
     public static void main(String[] args) {
         int[] arr={2,4,6,3,2,8};
