b02b275395ae3d506757b1ee1fb8b3c83893ff9c
imran1831 <imran807428@gmail.com>
2026-01-15 20:49:43 +0530

    Buy and Sell at  Maxprofit

--- BuySellStocks.java
+++ BuySellStocks.java
@@ -1,15 +1,36 @@
-public class BuySellStocks {
-    public static int maxPrice(int[] prices){
-        int profit = 0;
-        for (int i = 0; i < prices.length-1; i++) {
-            if (prices[i+1] > prices[i]) {
-                profit += prices[i+1] - prices[i];
-            }
+// public class BuySellStocks {
+//     public static int maxPrice(int[] prices){
+//         int profit = 0;
+//         for (int i = 0; i < prices.length-1; i++) {
+//             if (prices[i+1] > prices[i]) {
+//                 profit += prices[i+1] - prices[i];
+//             }
+//         }
+//         return profit;
+//     }
+//     public static void main(String[] args) {
+//         int[] arr={100, 180, 260, 310, 40, 535, 695};
+//         System.out.println(maxPrice(arr));
+//     }
+// }
+
+// ---------Max Profit at once---
+class BuySellStocks{
+public static int profitOnce(int prices[]){
+    int minPrice=Integer.MAX_VALUE;
+    int maxProfit=0;
+    for(int price:prices){
+        if(minPrice>price){
+            minPrice=price;
         }
-        return profit;
+        else if(price-minPrice>maxProfit){
+            maxProfit=price-minPrice;
+        }
     }
-    public static void main(String[] args) {
-        int[] arr={100, 180, 260, 310, 40, 535, 695};
-        System.out.println(maxPrice(arr));
-    }
+    return maxProfit;
+}
+public static void main(String[] args) {
+    int arr[]={7, 10, 1, 3, 6, 9, 2};
+    System.out.println(profitOnce(arr));
+}
 }
