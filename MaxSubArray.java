852664da4e77501fb59fb9ef07a8fa425736bd30
imran1831 <imran807428@gmail.com>
2026-01-17 21:27:29 +0530

    Kadenes Algorithm

---
+++ MaxSubArray.java
@@ -0,0 +1,15 @@
+public class MaxSubArray {
+    public static int maxSum(int[] arr,int k){
+        int i=0;
+        int j=k-1;
+        int oldSum=0;
+        while(j<arr.length){
+            
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={2, 1, 5, 1, 3, 2};
+        int k=3;
+        System.out.println(maxSum(arr,k));
+    }
+}
