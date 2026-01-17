77dcea6db466afa1e84cccf9de5c8d2239fc4100
imran1831 <imran807428@gmail.com>
2026-01-17 22:01:33 +0530

    Max SubArray

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -1,11 +1,16 @@
 public class MaxSubArray {
     public static int maxSum(int[] arr,int k){
-        int i=0;
-        int j=k-1;
-        int oldSum=0;
-        while(j<arr.length){
-            
+        if(arr.length<k) return -1;
+        int windowSum=0;
+        for(int i=0;i<k;i++){
+            windowSum+=arr[i];
         }
+        int maxSum=windowSum;
+        for(int i=k;i<arr.length;i++){
+            maxSum=maxSum-arr[i-k]+arr[i];
+            maxSum=Math.max(windowSum,maxSum);
+        }
+        return maxSum;
     }
     public static void main(String[] args) {
         int[] arr={2, 1, 5, 1, 3, 2};
