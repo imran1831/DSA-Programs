95e51876b83e45f22a8d2f562419eec87d054e4b
imran1831 <imran807428@gmail.com>
2026-01-20 23:20:16 +0530

    Kadanes initialize

---
+++ KadanesAlg.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class KadanesAlg {
+    public static void main(String[] args) {
+        int arr[]={2,5,7,-1,3,-8,5,4};
+        System.out.println(Arrays.toString(arr));
+    }
+}
