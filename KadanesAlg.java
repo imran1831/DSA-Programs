d9af90b01b3942df169a3a557959065ae63c47d4
imran1831 <imran807428@gmail.com>
2026-01-21 21:51:18 +0530

    updated

--- KadanesAlg.java
+++ KadanesAlg.java
@@ -1,8 +1,16 @@
-import java.util.Arrays;
 
 public class KadanesAlg {
+    public static int maxSubarraySum(int[] arr) {
+        int currentSum=arr[0];
+        int maxSum=arr[0];
+        for(int i=1;i<arr.length;i++){
+            currentSum=Math.max(arr[i],currentSum+arr[i]);
+            maxSum=Math.max(maxSum,currentSum);  
+        } 
+        return maxSum;
+    }
     public static void main(String[] args) {
-        int arr[]={2,5,7,-1,3,-8,5,4};
-        System.out.println(Arrays.toString(arr));
+        int arr[]={2, 3, -8, 7, -1, 2, 3};
+        System.out.println(maxSubarraySum(arr));
     }
 }
