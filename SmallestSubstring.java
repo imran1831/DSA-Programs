bdac32cf7f8be76c43eea3cc9c595a964870a838
imran1831 <imran807428@gmail.com>
2026-01-26 15:56:30 +0530

    Smallest Sub string

--- SmallestSubstring.java
+++ SmallestSubstring.java
@@ -1,6 +1,34 @@
+import java.util.HashMap;
 public class SmallestSubstring {
     public static String smallString(String s,String t){
-        return s;
+        if(s.length()<t.length())return "";
+        HashMap<Character,Integer>need=new HashMap<>();
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            need.put(ch,need.getOrDefault(ch,0)+1);
+        }
+        HashMap<Character,Integer>window=new HashMap<>();
+        int left=0;
+        int minLen=Integer.MAX_VALUE;
+        int formed=0;
+        int required=need.size();
+        int start=0;
+        for(int right=0;right<s.length();right++){
+            char c=s.charAt(right);
+            window.put(c,window.getOrDefault(c,0)+1);
+            if(need.containsKey(c)&&window.get(c).intValue()==need.get(c).intValue()) formed++;
+            while(formed==required){
+                if(right-left+1<minLen){
+                    minLen=right-left+1;
+                    start=left;
+                }
+                char leftChar=s.charAt(left);
+                window.put(leftChar,window.get(leftChar)-1);
+                if(need.containsKey(leftChar)&&need.get(leftChar)>window.get(leftChar))formed--;
+                left++;
+            }
+        }
+        return minLen==Integer.MAX_VALUE?"":s.substring(start,minLen+start);
     }
     public static void main(String[] args) {
         String s="aaabcbc";
