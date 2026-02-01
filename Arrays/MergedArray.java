dd51d219e47bc03b90535d89be2004944137e29d
imran1831 <imran807428@gmail.com>
2026-02-01 22:18:04 +0530

    Merged Sort

--- Arrays/MergedArray.java
+++ Arrays/MergedArray.java
@@ -1,18 +1,17 @@
+import java.util.*;
 public class MergedArray {
     public static int[] mergingArray(int[] nums1,int[] nums2,int m,int n){
+        int i=m-1;
+        int j=n-1;
+        int k=nums1.length-1;
         
-            for(int i=0;i<m;i++){
-                nums1[m++]=nums2[i];
-                
-            }
+        return nums1;
         }
-    }
     public static void main(String[] args) {
         int[] nums1={1,2,3,0,0,0};
         int[] nums2={2,5,6};
         int m=3; //Denotes the elements to be Added
         int n=3; //Denotes the elements to be removed
-        System.out.println(mergingArray(nums1,nums2,m,n));
-
+        System.out.println(Arrays.toString(mergingArray(nums1,nums2,m,n)));
     }
-}
+    }
