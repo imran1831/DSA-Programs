99b921ca7cb07b9c346084f01c9db065b0924f33
imran1831 <imran807428@gmail.com>
2026-02-03 23:42:56 +0530

    Adding Sum of All Elements in An Array

--- Arrays/ContainsDuplicate.java
+++ Arrays/ContainsDuplicate.java
@@ -1,19 +1,14 @@
 import java.util.HashSet;
-
 public class ContainsDuplicate {
-
     public static boolean containsDuplicate(int[] nums) {
         HashSet<Integer> seen = new HashSet<>(nums.length);
-
         for (int num : nums) {
             if (!seen.add(num)) { // already exists → duplicate
                 return true;
             }
         }
-
         return false;
     }
-
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 1};
 
---
+++ Arrays/SumOfAllEl.java
@@ -0,0 +1,16 @@
+public class SumOfAllEl {
+    public static int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        int[] result = runningSum(nums);
+        System.out.print("Running Sum: ");
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
