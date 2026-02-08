0d47b66c49121e8b25cead460c3b60d26c668041
imran1831 <imran807428@gmail.com>
2026-02-08 21:17:03 +0530

    Added Merged Sort optimized and complete code

--- Arrays/MergedArray.java
+++ Arrays/MergedArray.java
@@ -4,7 +4,17 @@ public class MergedArray {
         int i=m-1;
         int j=n-1;
         int k=nums1.length-1;
-        
+        while(i>=0&&j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[k]=nums1[i];
+                i--;
+            }
+            else{
+                nums1[k]=nums2[j];
+                j--;
+            }
+            k--;
+        }
         return nums1;
         }
     public static void main(String[] args) {
