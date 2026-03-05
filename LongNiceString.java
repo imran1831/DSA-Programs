10583c90d5b0b53734ae80b3d9765bd00093f717
imran1831 <imran807428@gmail.com>
2026-03-05 23:58:54 +0530

    Added Longest Nice subString using Sliding window pattern

---
+++ LongNiceString.java
@@ -0,0 +1,12 @@
+public class LongNiceString {
+
+    public static String longestNiceSubstring(String s) {
+        // Implementation for finding the longest nice substring
+        return "";
+    }
+    
+    public static void main(String[] args) {
+        String s="YazaAay";
+        System.out.println(longestNiceSubstring(s));
+    }
+}
