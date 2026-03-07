9f1759fe2dcb47a523f0a8e0aa2ba978f5fccf00
imran1831 <imran807428@gmail.com>
2026-03-07 23:13:18 +0530

    Product Except Itself program using prefix sum pattern

---
+++ Arrays/ProdExceptItself.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+public class ProdExceptItself {
+        public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        int n = nums.length;
+        int[] result = new int[n];
+        int prefix = 1;
+        for(int i=0;i<n;i++){
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+        int suffix = 1;
+        for(int i=n-1;i>=0;i--){
+            result[i] *= suffix;
+            suffix *= nums[i];
+        }
+        System.out.println(Arrays.toString(result));
+    }
+}
