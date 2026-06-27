b1dfd39b9d0672dd0dfcacebd2dc29a35f3cf666
imran1831 <imran807428@gmail.com>
2026-06-27 12:00:00 +0530

    Set the spaces

--- Arrays/MinHeight.java
+++ Arrays/MinHeight.java
@@ -7,10 +7,8 @@ public class MinHeight {
         Arrays.sort(arr);
 
         int ans = arr[n - 1] - arr[0];
-
         for (int i = 1; i < n; i++) {
             if (arr[i] - k < 0) continue;
-
             int minHeight = Math.min(arr[0] + k, arr[i] - k);
             int maxHeight = Math.max(arr[i - 1] + k, arr[n - 1] - k);
             ans = Math.min(ans, maxHeight - minHeight);
