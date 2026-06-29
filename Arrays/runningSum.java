54f09b15dd1cfe49ddb3c58a481a55a798174dec
imran1831 <imran807428@gmail.com>
2026-06-29 12:00:00 +0530

    correcting the code

--- Arrays/runningSum.java
+++ Arrays/runningSum.java
@@ -1,10 +1,8 @@
 import java.util.Arrays;
 public class runningSum {
     public static int[] sum(int[] nums) {
-
         int[] prefix = new int[nums.length];
         prefix[0] = nums[0];
-
         for (int i = 1; i < nums.length; i++) {
             prefix[i] = prefix[i - 1] + nums[i];
         }
